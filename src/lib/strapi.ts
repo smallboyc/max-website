299133e68c6e0c9892498c5a61548b64cd5729dd
smallboyc <maxencedup44@gmail.com>
2024-02-20 10:53:37 +0100

    fix : one day revalidation set

--- src/lib/strapi.ts
+++ src/lib/strapi.ts
@@ -2,7 +2,7 @@ export async function getData(path: string) {
   const res = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_URL}/api/${path}`, {
     method: "GET",
     headers: { "Content-Type": "application/json" },
-    next: { revalidate: 60 },
+    next: { revalidate: 86400 },
   });
   return res.json();
 }
