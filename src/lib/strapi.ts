f01bd9fdbd5f8c221841245b05a693fb3ee4ba85
smallboyc <maxencedup44@gmail.com>
2024-02-20 10:00:53 +0100

    fix : set revalidate

--- src/lib/strapi.ts
+++ src/lib/strapi.ts
@@ -2,6 +2,7 @@ export async function getData(path: string) {
   const res = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_URL}/api/${path}`, {
     method: "GET",
     headers: { "Content-Type": "application/json" },
+    next: { revalidate: 60 },
   });
   return res.json();
 }
