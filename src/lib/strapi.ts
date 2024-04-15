eab9b453c7aad46ea9d638f738630f49ce2371b2
smallboyc <maxencedup44@gmail.com>
2024-04-15 11:49:32 +0200

    fix : revalidate edit to 1h

--- src/lib/strapi.ts
+++ src/lib/strapi.ts
@@ -2,7 +2,7 @@ export async function getData(path: string) {
   const res = await fetch(`${process.env.NEXT_PUBLIC_STRAPI_URL}/api/${path}`, {
     method: "GET",
     headers: { "Content-Type": "application/json" },
-    next: { revalidate: 86400 },
+    next: { revalidate: 3600 },
   });
   return res.json();
 }
